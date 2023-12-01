5df3a7eaa54c45d332e20d76f88a1f9ca4d12a20
anonym0uz <102824282+anonymouz-trash@users.noreply.github.com>
2023-12-01 19:22:15 +0100

    Delete git-pull-arch-updater.sh

--- git-pull-arch-updater.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ~
-mkdir git
-cd git
-git config --global credential.helper store
-git clone https://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@github.com/anonym0uz-trash/arch-updater.git
-cd arch-updater
-bash ./arch-updater.sh
