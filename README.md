d6b9425b9aa86cf4f37be9f858224d7885431bf3
anonym0uz <anonym0uz.trash@gmail.com>
2024-06-03 15:44:10 +0200

    uploaded latest release

---
+++ README.md
@@ -0,0 +1,29 @@
+# Arch Linux Updater Script
+
+This is a try to fully automate the Arch Linux maintainance and get a more knowledge of how Arch Linux works.
+My setup:  Arch Linux (ofc ^^), KDE Desktop (sometimes GNOME) and AMD (Desktop) / Nvidia (Laptop) graphics.
+
+Feel free to use any of the code in this repo for your own.
+I highly encourage you to read through the script before you using it!
+As you know: No backup, no mercy. ;)
+
+# Changelog
+
+v2.0 released :D :
+- ditched dialog as TUI
+- created an even simplier menu just with echo and case in a do-while-loop
+- full rewrite
+- re-organize categories & assets
+- make it more KISS
+
+# Disclaimer
+
+THIS IS WORK IN PROGRESS!!
+
+I'm not responsible for any damage and I highly recommend to see through the entire project/code before using it!
+The whole project is adjusted to my needs.
+
+# Screenshots
+![Arch Linux Updater - Main Menu](https://github.com/anonymouz-trash/arch-updater/blob/main/screenshots/arch-updater_mainmenu.png?raw=true)
+![Arch Linux Updater - Main Menu](https://github.com/anonymouz-trash/arch-updater/blob/main/screenshots/arch-updater_customization.png?raw=true)
+![Arch Linux Updater - Main Menu](https://github.com/anonymouz-trash/arch-updater/blob/main/screenshots/arch-updater_optimizations.png?raw=true)
---
+++ assets/cust_whitesur.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+pwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $pwd
+de=$XDG_CURRENT_DESKTOP
+
+# Check if WhiteSur got pulled and is up to date
+if [ -d "WhiteSur-gtk-theme" ]; then
+	read -p "Do you want to (r)emove or just update it? [r/U] " input
+	if [[ $input == "r" ]]; then
+		sudo ./WhiteSur-gtk-theme/install.sh -r
+		rm -rf ./WhiteSur-gtk-theme
+		if [[ -d WhiteSur-kde ]]; then
+            rm -rf ./WhiteSur-kde
+			sudo rm -rf /usr/share/themes/WhiteSur*
+			sudo rm -rf /usr/share/sddm/themes/WhiteSur*
+		fi
+	else
+		cd WhiteSur-gtk-theme
+		git pull
+		cd ..
+		if [[ -d WhiteSur-kde ]]; then
+			cd WhiteSur-kde
+			git pull
+			cd ..
+		fi
+	fi
+else
+	git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
+	if [[ ${de,,} =~ "kde" ]]; then
+		git clone https://github.com/vinceliuice/WhiteSur-kde.git
+	fi
+fi
+
+# Installation
+
+
+
+if [[ ${de,,} =~ "gnome" ]]; then
+	# Get actual set background of GNOME
+	curBg=$(gsettings get org.gnome.desktop.background picture-uri)
+	first=${curBg/\'file:\/\//}
+	second=${first/\'/}
+	curBg=$second
+	./WhiteSur-gtk-theme/install.sh -l -m -o normal -c Dark -t all -i arch -b $curBg -N glassy
+	sudo ./WhiteSur-gtk-theme/tweaks.sh -g -r
+	sudo ./WhiteSur-gtk-theme/tweaks.sh -g -b $curBg -c Dark -i arch
+elif [[ ${de,,} =~ "kde" ]]; then
+	sudo ./WhiteSur-kde/install.sh -c dark --opaque
+	sudo ./WhiteSur-kde/sddm/install.sh
+fi
+
+echo "Do you want to remove previously downloaded files? "
+read -p "Type (n) or just press [Enter] if you want to update in the future [y/N] " input
+
+if [[ $input == "y" ]]; then
+	rm -rf WhiteSur-gtk-theme
+	if [ -d "WhiteSur-kde" ]; then
+		rm -rf WhiteSur-kde
+	fi
+fi
