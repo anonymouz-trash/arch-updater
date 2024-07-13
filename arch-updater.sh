88241744aacecc2da59563376e957d42bf7777d6
anonym0uz <anonym0uz@noreply.localhost>
2024-07-13 21:03:04 +0200

    Update arch-updater.sh

--- arch-updater.sh
+++ arch-updater.sh
@@ -4,7 +4,6 @@
 # This is useful if you plan to start the script via global hotkey
 # because of the assets and the the use of relative paths
 pwd=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd ${pwd}
 
 ### Include required functions
 source ./config/colors.sh
