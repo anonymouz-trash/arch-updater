f27833546bf6ad54b5c9afe1686f98ead5642136
anonym0uz <anonym0uz@noreply.localhost>
2025-01-13 21:20:53 +0100

    Update assets/opt_wireguard-vpn.sh

--- assets/opt_wireguard-vpn.sh
+++ assets/opt_wireguard-vpn.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/bash
+
+# Specifiy the Wireguard interface (config) name
 WG_INTERFACE_NAME=wg0
-if [ $(ip l | grep "$WG_INTERFACE_NAME" | wc -l > /dev/null) -ne 0 ]; then
-  pkexec wg-quick down wg0
-  notify-send -t 5000 -i "$ICON" "Wireguard VPN" "$WG_INTERFACE_NAME is not connected"
+
+# First check if the output of 'ip link' contains the Wireguard interface
+if [ $(ip l | grep "$WG_INTERFACE_NAME" | wc -l) -ne 0 ]; then
+
+  # If the result is true, then it will try to shutdown the Wireguard interface and redirect the error channel
+  # to STDOUT and put it into the $TMP variable (if there went anything wrong).
+  if pkexec wg-quick down wg0 2>&1 | tee $TMP; then
+    
+    # If anything went ok then a notification is shown like below.
+    notify-send -t 5000 -i "dialog-information" "Wireguard VPN" "$WG_INTERFACE_NAME is not connected"
+  else
+
+    # If there went anything wrong then it will be shown in a warning dialog contain the error message.    
+    notify-send -u critical -t 5000 -i "dialog-warning" "Wireguard VPN" "$TMP"
+    rm $TMP
+  fi
   exit 1
 else
-  pkexec wg-quick up wg0
-  notify-send -t 5000 -i "$ICON" "Wireguard VPN" "$WG_INTERFACE_NAME is connected"
+
+  # Same as above but the order of operation is the opposite.
+  if pkexec wg-quick up wg0 2>&1 | tee $TMP; then
+    notify-send -t 5000 -i "dialog-information" "Wireguard VPN" "$WG_INTERFACE_NAME is connected"
+  else
+    notify-send -u critical -t 5000 -i "dialog-warning" "Wireguard VPN" "$TMP"
+    rm $TMP
+  fi
   exit 1
 fi
