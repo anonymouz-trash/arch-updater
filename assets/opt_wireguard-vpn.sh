02f416835eb8c3311dda028cb898a4226b1d4ccb
anonym0uz <anonym0uz-user@proton.me>
2026-05-11 19:36:15 +0200

    Update: Wireguard script now includes adding secure dns on normal wlan/ethernet

--- assets/opt_wireguard-vpn.sh
+++ assets/opt_wireguard-vpn.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/bash
 
-# Specifiy the Wireguard interface (config) name
-WG_INTERFACE_NAME=wg0
+WG_INTERFACE_NAME="wg0" # <-- Set your correct configuration filename from /etc/wireguard, e.g. wg0.conf
+WIFI_INTERFACE="wlan0"  # <-- Set your correct wifi device name. (ip -c a)
 
-# First check if the output of 'ip link' contains the Wireguard interface
-if [ $(ip l | grep "$WG_INTERFACE_NAME" | wc -l) -eq 0 ]; then
-	# Wireguard gets started.
-	# && means success notify
-	# || means any error
-	pkexec wg-quick up wg0 && notify-send -t 5000 -i "dialog-information" "Wireguard VPN" "$WG_INTERFACE_NAME is connected..." || notify-send -u critical -t 5000 -i "dialog-warning" "Wireguard VPN" "Something went wrong!"
+enable_secure_dns() {
+
+    # dnsforge and quad9 DoT
+    sudo resolvectl dns "$WIFI_INTERFACE" 176.9.93.198 9.9.9.9
+    sudo resolvectl dnsovertls "$WIFI_INTERFACE" yes
+
+}
+
+disable_secure_dns() {
+
+    # DHCP / Router DNS reset
+    sudo resolvectl revert "$WIFI_INTERFACE"
+
+}
+
+# Check if WireGuard is active
+if ip link show "$WG_INTERFACE_NAME" &>/dev/null; then
+
+    # deactivate VPN
+    pkexec wg-quick down "$WG_INTERFACE_NAME" && \
+    enable_secure_dns && \
+    notify-send -t 5000 -i "dialog-information" \
+    "WireGuard VPN" \
+    "$WG_INTERFACE_NAME disconnected | Secure DNS enabled"
 
 else
-	pkexec wg-quick down wg0 && notify-send -t 5000 -i "dialog-information" "Wireguard VPN" "$WG_INTERFACE_NAME is disconnected..." || notify-send -u critical -t 5000 -i "dialog-warning" "Wireguard VPN" "Something went wrong!"
+
+    # activate VPN
+    pkexec wg-quick up "$WG_INTERFACE_NAME" && \
+    disable_secure_dns && \
+    notify-send -t 5000 -i "dialog-information" \
+    "WireGuard VPN" \
+    "$WG_INTERFACE_NAME connected | Secure DNS disabled"
+
 fi
